3eb4bfda5a5512256125ba6dd90eafc683526f35
RNSSUNIL VEDA <vrnssunil@gmail.com>
2018-07-03 09:23:40 +0530

    Update urls.py

--- Test1/Details/urls.py
+++ Test1/Details/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url,include
-from .rest_api import test,getlist,get,post
+from .rest_api import test,getlist,post
 urlpatterns = [
 url(r'^test/',test),
 url('list/', getlist),
-url(r'^get/(?P<_num>[0-99]+)/',get),
-url(r'^post/(?P<_values>[0-99,-]+)/',post)
+rl(r'^post/',add_cal)
+
 ]
