c96f36e15514b26160ab79c653e29efd509a251e
RNSSUNIL VEDA <vrnssunil@gmail.com>
2018-07-03 15:57:06 +0530

    Update rest_api.py

--- Test1/Details/rest_api.py
+++ Test1/Details/rest_api.py
@@ -25,3 +25,39 @@ def add_cal(request):
     square = request.data.get("square", None)
     cal = Testcalc.objects.create(number=number,square=square)
     return Response({'message' : 'Square updated or created'})
+@api_view(['GET', 'POST', 'PUT'])
+def post (request,_number):
+
+    if request.method == 'GET':
+        mul = Testcalc.objects.filter(number=_number)
+        serializer = TestcalcSerializer(mul, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TestcalcSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status= 301)
+
+    elif request.method == 'PUT':
+        print (request.data)
+        mul = Testcalc.objects.filter(number=request.data["number"])
+        print (mul)
+        mul.update(square=request.data["square"])
+        serializer = TestcalcSerializer(data=mul, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= 301)
+@api_view(['DELETE'])
+def apinum(request,_number):
+	print(request.data)
+	num = Testcalc.objects.filter(number=_number).delete()
+	print (num)
+#	num.delete(number=request.data["number"])
+	serializer = TestcalcSerializer(data=num, many=True)
+	if serializer.is_valid():
+		serializer.save()
+		return Response(serializer.data, status= 301)
+
