89e2a69aac790181d4a7abb8e51c07396810e41a
RNSSUNIL VEDA <vrnssunil@gmail.com>
2018-07-03 09:22:15 +0530

    Update rest_api.py

--- Test1/Details/rest_api.py
+++ Test1/Details/rest_api.py
@@ -4,12 +4,7 @@ from django.shortcuts import render
 from .serializers import TestcalcSerializer
 from .models import Testcalc
 from django.http import JsonResponse
-import MySQLdb
 
-HOST = 'localhost'
-USER = 'root'
-PASSWORD = 'vydehi9'
-DATABASE = 'testing'
 @api_view(['GET']) #calling api_view by using decorators with help of GET
 def test(request):#TEST The function printing in Json format or not
 	return Response({'Everything': 'Fair and Lovely', 'Welcome': 'everybody'})
@@ -23,42 +18,10 @@ def getlist(request):
     else:
         return Response("No Data")
 
-@api_view(['GET'])
-def get(request, _num):
-    _mar = Testcalc.objects.filter(number = _num)
-    if _mar:
-        serializer = TestcalcSerializer(_mar, many = True)
-        return Response(serializer.data)
-    else:
-        _sq = int(_num) * int(_num)
-        db=MySQLdb.connect(host = HOST, user = USER, passwd = PASSWORD, db = DATABASE)
-        cursor = db.cursor()
-        cursor.execute('INSERT INTO Details_testcalc  (number, square) values(%s, %s)',(float(_num),float(_sq)))
-        db.commit()
-        cursor.close()
-        print (_sq)
-        return Response({'Message': "New number and it's square inserted", 'Number': _num, 'Square': _sq})
 
-
-
-
-@api_view(['GET'])
-def post(request, _values):
-    _num, _sq = _values.split('-')
-    _mar = Testcalc.objects.filter(number = _num)
-    if _mar:
-        db=MySQLdb.connect(host = HOST, user = USER, passwd = PASSWORD, db = DATABASE)
-        cursor = db.cursor()
-        cursor.execute('UPDATE Details_testcalc  SET square = %s WHERE number = %s',(float(_sq),float(_num)))
-        db.commit()
-        cursor.close()
-        print (_sq)
-        return Response({'Message': 'Square of the given number is updated', 'Number': _num, 'Square': _sq})
-    else:
-        db=MySQLdb.connect(host = HOST, user = USER, passwd = PASSWORD, db = DATABASE)
-        cursor = db.cursor()
-        cursor.execute('INSERT INTO Details_testcalc  (number, square) values(%s, %s)',(float(_num),float(_sq)))
-        db.commit()
-        cursor.close()
-        print (_sq)
-        return Response({'Message': 'New number and square inserted', 'Number': _num, 'Square': _sq})
+@api_view(['POST'])
+def add_cal(request):
+    number = request.data.get("number", None)
+    square = request.data.get("square", None)
+    cal = Testcalc.objects.create(number=number,square=square)
+    return Response({'message' : 'Square updated or created'})
